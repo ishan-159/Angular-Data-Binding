6f68a29ed428e446cfc4686ff13dc2c280d70d4f
ishan-159 <ishantiwari159@gmail.com>
2025-03-20 23:25:04 +0530

    [Ishan] Add.Open BridgeLabz website on clicking BridgeLabz logo.

--- DataBinding/src/app/app.component.ts
+++ DataBinding/src/app/app.component.ts
@@ -9,8 +9,12 @@ export class AppComponent {
   title = 'DataBinding';
   message: string;
   logoUrl = 'https://gumlet.assettype.com/freepressjournal%2F2021-06%2F32af7f9f-b603-4e12-8340-ba2425bb5ee8%2FBridgeLabz.PNG';
+  siteUrl = 'https://www.bridgelabz.com';
 
   constructor() {
     this.message = 'Hello from BridgeLabz';
+  }
+  openBridgeLabzSite() {
+    window.open(this.siteUrl, '_blank'); 
   }
 }
