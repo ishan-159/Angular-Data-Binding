3a5bbc557d1dbce6e4b608829cb6ceb64dccd098
ishan-159 <ishantiwari159@gmail.com>
2025-03-20 23:10:04 +0530

    [Ishan] Add.Added Logo.

--- DataBinding/src/app/app.component.ts
+++ DataBinding/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'DataBinding';
   message: string;
+  logoUrl = 'https://gumlet.assettype.com/freepressjournal%2F2021-06%2F32af7f9f-b603-4e12-8340-ba2425bb5ee8%2FBridgeLabz.PNG';
 
   constructor() {
     this.message = 'Hello from BridgeLabz';
